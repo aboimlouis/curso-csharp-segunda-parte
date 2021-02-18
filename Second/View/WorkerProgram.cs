284557409bc165f865e95117b19ef8b16cf8b238
Louis Aboim <aboimlouis@gmail.com>
2021-02-18 01:30:30 -0300

    Correcoes da primeira atividade ENUMS

--- Second/View/WorkerProgram.cs
+++ Second/View/WorkerProgram.cs
@@ -3,44 +3,33 @@ using System.Collections.Generic;
 using System.Text;
 using Second.Entities;
 using Second.Entities.Enums;
-
+using System.Globalization;
 namespace Second.View
 {
     class WorkerProgram
     {
         public static void Display()
         {
-            string name;
-            Department department;
-            WorkerLevel level;
-            double baseSalary;
-            int numberOfContracts;
-            DateTime calculateIncomeDate;
-
 
             Console.Write("Enter Department's name: ");
-            department = new Department(Console.ReadLine());
-            Console.WriteLine();
+            string deptName = Console.ReadLine(); //Alterei, pois imagino que depois seja ideal separar o view da criação de objetos (?)
 
-            Console.WriteLine("Enter worker data:");
+            Console.WriteLine("Enter worker data -");
 
             Console.Write("Name: ");
-            name = Console.ReadLine();
-            Console.WriteLine();
+            string name = Console.ReadLine();
 
             Console.Write("Level (Junior/MidLevel/Senior): ");
-            level = Enum.Parse<WorkerLevel>(Console.ReadLine());
-            Console.WriteLine();
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
 
             Console.Write("Base salary: ");
-            baseSalary = double.Parse(Console.ReadLine());
-            Console.WriteLine();
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
 
             Console.Write("How many contracts to this worker: ");
-            numberOfContracts = int.Parse(Console.ReadLine());
-            Console.WriteLine();
+            int numberOfContracts = int.Parse(Console.ReadLine());
 
-            Worker worker = new Worker(name, level, baseSalary, department);
+            Department dept = new Department(deptName);
+            Worker worker = new Worker(name, level, baseSalary, dept);
 
             for(int i = 0; i < numberOfContracts; i++)
             {
@@ -48,27 +37,29 @@ namespace Second.View
                 double valuePerHour;
                 int hours;
 
-                Console.WriteLine("Enter #" + i + " contract data:");
+                Console.WriteLine($"Enter #{i+1} contract data:");
 
                 Console.Write("Date (DD/MM/YYYY): ");
                 date = DateTime.Parse(Console.ReadLine());
-                Console.WriteLine();
 
                 Console.Write("Value per hour: ");
                 valuePerHour = double.Parse(Console.ReadLine());
-                Console.WriteLine();
 
                 Console.Write("Duration (hours): ");
-                hours = int.Parse(Console.ReadLine());
-                Console.WriteLine();
+                hours = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-                worker.AddContract(new HourContract(date, valuePerHour, hours));
+                worker.AddContract(new HourContract(date, valuePerHour, hours)); //ele separa a criação do objeto contract e a adição dele na lista
             }
 
             Console.Write("Enter month and year to calculate income (MM/YYYY): ");
-            calculateIncomeDate = DateTime.Parse(Console.ReadLine());
+            DateTime calculateIncomeDate = DateTime.Parse(Console.ReadLine());
 
-            Console.WriteLine("Income for " + calculateIncomeDate + ": " + worker.Income(calculateIncomeDate.Year, calculateIncomeDate.Month));
+
+            Console.WriteLine("Name: " + worker.Name);
+            Console.WriteLine("Department: " + worker.Department.Name);
+            Console.WriteLine("Income for " 
+                + calculateIncomeDate.Month + "/" + calculateIncomeDate.Year + ": " 
+                + worker.Income(calculateIncomeDate.Year, calculateIncomeDate.Month).ToString("F2", CultureInfo.InvariantCulture));
 
         }
     }
