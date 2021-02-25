c15245c0625dbba2d79201710296ffcf4eea216f
Louis Aboim <aboimlouis@gmail.com>
2021-02-25 17:20:01 -0300

    Adicionado segunda aula de inheritance

--- Second/Entities/Account/Account.cs
+++ Second/Entities/Account/Account.cs
@@ -6,9 +6,9 @@ namespace Second.Entities.Account
 {
     class Account
     {
-        public int Number { get; set; }
-        public string Holder { get; set; }
-        public double Balance { get; set; }
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
 
         public Account()
         {
---
+++ Second/Entities/Account/SavingsAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Second.Entities.Account
+{
+    class SavingsAccount : Account
+    {
+        public double Interest { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interest) : base(number, holder, balance)
+        {
+            Interest = interest;
+        }
+
+
+        public void UpdateBalance()
+        {
+
+        }
+    }
+}
